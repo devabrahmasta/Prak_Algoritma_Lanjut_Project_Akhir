8b80f9b51abbfff043f1e81242af7fdea2e8e7a5
Ananda Rizky <anandarizkysetyanu29@gmail.com>
2025-02-24 15:03:16 +0700

    Menambah struct & mengubah bagian login

--- pa_prak_algo.cpp
+++ pa_prak_algo.cpp
@@ -2,13 +2,35 @@
 #include <iomanip>
 using namespace std;
 
+
+struct akun_pengguna{
+    int ID;
+    string username;
+    string password;
+};
+
+akun_pengguna akun[1001];
+int jumlahakun=0;
 char pilihMenu;
-string username, password, ulangMenu;
+string username, password;
+string ulangMenu;
 bool kesempatan = false;
 int kesempatanlogin = 3;
 
-void errorInput()
-{
+//Fungsi
+void errorInput();
+void loginPage();
+void menaruhItems();
+void mengambilItems();
+void mencariItems();
+void menuPage();
+
+
+int main(){
+    loginPage();
+}
+
+void errorInput(){
     system("pause");
     cout << setfill('=') << setw(40) << "=" << endl;
     cout << setfill(' ') << setw(4) << " " << "Mohon maaf Anda salah input" << endl
@@ -17,41 +39,62 @@ void errorInput()
     cout << setfill(' ') << setw(4) << " " << "Mohon masukkan input kembali" << endl;
     cout << setfill('=') << setw(40) << "=" << endl;
 }
-void loginPage()
-{
+
+void loginPage(){
+    bool check_username=false;
+    bool check_password=false;
     cout << setfill('=') << setw(40) << "=" << endl;
     cout << "username : ";
     getline(cin, username);
     cout << "password : ";
     cin >> password;
+
+    for(int i=0;i<=jumlahakun;i++){
+        if(username==akun[i].username) check_username=true;
+        if(password==akun[i].password) check_password=true;
+        if(check_password==true&&check_username==true){
+            break;
+        }
+        else {
+            check_password=false;
+            check_username=false;
+        }
+    }
+    if(check_password==true&&check_username==true){
+        cout<<"Login Berhasil";
+        cin.ignore();
+        getchar();
+        menuPage();
+    }
+    else {
+        cout<<"Username atau Password salah. Tolong masukan yang benar atau buat akun dahulu"<<endl;
+        cin.ignore();
+        getchar();
+    }
 }
 
-void menaruhItems()
-{
+void menaruhItems(){
     system("cls");
     cout << setfill('=') << setw(40) << "=" << endl;
     cout << setfill(' ') << setw(8) << " " << "Menaruh Items" << endl;
     cout << setfill('=') << setw(40) << "=" << endl;
-};
+}
 
-void mengambilItems()
-{
+void mengambilItems(){
     system("cls");
     cout << setfill('=') << setw(40) << "=" << endl;
     cout << setfill(' ') << setw(8) << " " << "Mengambil Items" << endl;
     cout << setfill('=') << setw(40) << "=" << endl;
-};
+}
 
-void mencariItems()
-{
+void mencariItems(){
     system("cls");
     cout << setfill('=') << setw(40) << "=" << endl;
     cout << setfill(' ') << setw(8) << " " << "Mencari Items" << endl;
     cout << setfill('=') << setw(40) << "=" << endl;
-};
+}
 
-void menuPage()
-{
+void menuPage(){
     system("cls");
     cout << setfill('=') << setw(40) << "=" << endl;
     cout << setfill(' ') << setw(8) << " " << "SELAMAT DATANG DI GUDANG" << endl;
@@ -101,10 +144,4 @@ void menuPage()
         system("pause");
         menuPage();
     }
-};
-
-int main()
-{
-    loginPage();
-    menuPage();
-};
+}
