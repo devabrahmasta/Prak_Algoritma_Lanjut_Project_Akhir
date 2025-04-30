2dc603a0444914fb1e1bcb73bf48845dd63d74f7
Ananda Rizky <anandarizkysetyanu29@gmail.com>
2025-04-30 19:38:35 +0700

    salin dari main

--- pa_prak_algo.cpp
+++ pa_prak_algo.cpp
@@ -2,16 +2,39 @@
 #include <iomanip>
 using namespace std;
 
+struct gudang
+{
+    string namaBarang[1001];
+    string idBarang[1001];
+};
+
 struct akun_pengguna
 {
     int ID;
     string username;
     string password;
+    gudang namaBarang;
 };
 
-//deklarasi global
+// penyimpanan gudang
+// string barang[1001] = { // diganti nanti
+//     "Laptop",
+//     "Printer",
+//     "Meja",
+//     "Kursi",
+//     "Rak Buku",
+//     "Proyektor",
+//     "Kabel Listrik",
+//     "Papan Tulis",
+//     "AC (Air Conditioner)",
+//     "Lampu"
+
+// };
+
+// deklarasi global
 akun_pengguna akun[1001];
-int jumlahakun = 0;
+akun_pengguna pengguna;
+int jumlahakun = 0, q = 0;
 char pilihMenu;
 string username, password;
 string ulangMenu;
@@ -21,15 +44,16 @@ int kesempatanlogin = 3;
 // Fungsi
 void errorInput();
 void loginPage();
-void menaruhItems();
+void menambahItems();
 void mengambilItems();
 void mencariItems();
+void historiItems();
 void menuPage();
 
 int main()
 {
     system("cls");
-    //loginPage();
+    // loginPage();
     menuPage();
 }
 
@@ -90,15 +114,15 @@ void loginPage()
         getchar();
         if (optionFalse == 1)
             loginPage();
-        else if(optionFalse == 2) // registerpage
+        else if (optionFalse == 2) // registerpage
             loginPage();
-        
-
     }
 }
 
-void menaruhItems()
+void menambahItems()
 {
+    int quantity;
+    bool add = true;
     system("cls");
     cout << setfill('=') << setw(40) << "=" << endl;
     cout << setfill(' ') << setw(8) << " " << "Menambah Items" << endl;
@@ -107,17 +131,15 @@ void menaruhItems()
     {
         cout << setfill('=') << setw(40) << "=" << endl;
         cout << "Ingin menambahkan berapa barang : ";
-        cin.ignore();
         cin >> quantity;
-        cin.ignore();
         for (int i = 0; i < quantity; i++)
         {
             cout << "Masukkan nama barang ke-" << q + 1 << " Anda : ";
-            
-            getline(cin>>ws,pengguna.namaBarang.namaBarang[q]);
+            cin.ignore();
+            getline(cin, pengguna.namaBarang.namaBarang[q]);
             q++;
         }
-        
+
         cout << "\nIngin menambah barang kembali? (y/n): ";
         cin >> ulangMenu;
     } while (ulangMenu == "Y" || ulangMenu == "y");
@@ -140,15 +162,41 @@ void mencariItems()
     cout << setfill('=') << setw(40) << "=" << endl;
 }
 
+void menampilkanItems()
+{
+    // akun_pengguna pengguna;
+    system("cls");
+    cout << setfill('=') << setw(40) << "=" << endl;
+    cout << setfill(' ') << setw(8) << " " << "Menampilkan Items" << endl;
+    cout << setfill('=') << setw(40) << "=" << endl;
+    
+    for (int i = 0; i < q; i++)
+    {
+        cout << i + 1 << ". " << pengguna.namaBarang.namaBarang[i] << endl;
+    }
+    cout << setfill('=') << setw(40) << "=" << endl;
+}
+
+void historiItems()
+{
+    system("cls");
+    cout << setfill('=') << setw(40) << "=" << endl;
+    cout << setfill(' ') << setw(8) << " " << "Histori Items" << endl;
+    cout << setfill('=') << setw(40) << "=" << endl;
+}
+
 void menuPage()
 {
     system("cls");
     cout << setfill('=') << setw(40) << "=" << endl;
     cout << setfill(' ') << setw(8) << " " << "SELAMAT DATANG DI GUDANG" << endl;
     cout << setfill('=') << setw(40) << "=" << endl;
-    cout << "1. Menaruh item" << endl;
+    cout << "0. Logout" << endl;
+    cout << "1. Menambah item" << endl;
     cout << "2. Mengambil item" << endl;
     cout << "3. Mencari item" << endl;
+    cout << "4. Menampilkan item" << endl;
+    cout << "5. Histori" << endl;
     cout << setfill('=') << setw(40) << "=" << endl;
     cout << "Mau ngapain    : ";
     cin >> pilihMenu;
@@ -160,12 +208,23 @@ void menuPage()
         main();
         system("pause");
         break;
+    case '1':
+        menambahItems();
+        break;
     case '2':
         mengambilItems();
         system("pause");
         break;
     case '3':
         mencariItems();
+        system("pause");
+        break;
+    case '4':
+        menampilkanItems();
+        system("pause");
+        break;
+    case '5':
+        historiItems();
         system("pause");
         break;
 
@@ -175,7 +234,8 @@ void menuPage()
         menuPage();
         break;
     }
-    cout << "ingin kembali? (y/n) : ";
+    cin.ignore();
+    cout << "Kembali ke menu? (y/n) : ";
     cin >> ulangMenu;
     if (ulangMenu == "Y" || ulangMenu == "y")
     {
